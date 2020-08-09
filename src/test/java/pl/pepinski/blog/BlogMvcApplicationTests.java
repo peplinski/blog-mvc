a20bad8cf24a260825f3a22c74b41ef7d3e96e34
Driver <peplinskikrzysztof@gmail.com>
2020-08-09 12:15:16 +0200

    initial coomit

---
+++ src/test/java/pl/pepinski/blog/BlogMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pepinski.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
